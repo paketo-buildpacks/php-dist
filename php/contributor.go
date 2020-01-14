af2cd24aac01254e53731adf4eab7381da679588
Mark DeLillo <mdelillo@pivotal.io>
2020-01-14 11:36:30 -0500

    Don't strip any components when extracting dependencies
    * Fixes the case where a tarball had a flat structure and no leading ./
    
    [#170366955]

--- php/contributor.go
+++ php/contributor.go
@@ -74,7 +74,7 @@ func NewContributor(context build.Build) (c Contributor, willContribute bool, er
 func (c Contributor) Contribute() error {
 	return c.phpLayer.Contribute(func(artifact string, layer layers.DependencyLayer) error {
 		layer.Logger.SubsequentLine("Expanding to %s", layer.Root)
-		if err := helper.ExtractTarGz(artifact, layer.Root, 1); err != nil {
+		if err := helper.ExtractTarGz(artifact, layer.Root, 0); err != nil {
 			return err
 		}
 
