14bad8cae2515171fd890c4caad373f862f09f3a
CF Buildpacks Team CI Server <cf-buildpacks-eng@pivotal.io>
2020-02-07 19:22:31 +0000

    Update CNB tools

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,9 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 # shellcheck source=.util/tools.sh
 source "${PWD}/scripts/.util/tools.sh"
 
-util::tools::install \
-    --directory "${PWD}/.bin" \
-    --pack-version "latest"
+util::tools::packager::install "${PWD}/.bin"
 
 PACKAGE_DIR=${PACKAGE_DIR:-"${PWD##*/}_$(openssl rand -hex 4)"}
 
@@ -17,23 +15,24 @@ args=".bin/packager -uncached"
 
 while getopts "acv:" arg
 do
-    case $arg in
+    case "${arg}" in
     a) archive=true;;
     c) cached=true;;
     v) version="${OPTARG}";;
+    *) echo "unknown argument ${arg}"; exit 1;;
     esac
 done
 
-if [[ ! -z "$cached" ]]; then #package as cached
+if [[ -n "${cached:-}" ]]; then #package as cached
     full_path="$full_path-cached"
     args=".bin/packager"
 fi
 
-if [[ ! -z "$archive" ]]; then #package as archive
+if [[ -n "${archive:-}" ]]; then #package as archive
     args="${args} -archive"
 fi
 
-if [[ -z "$version" ]]; then #version not provided, use latest git tag
+if [[ -z "${version:-}" ]]; then #version not provided, use latest git tag
     git_tag=$(git describe --abbrev=0 --tags)
     version=${git_tag:1}
 fi
@@ -42,7 +41,7 @@ args="${args} -version ${version}"
 
 eval "${args}" "${full_path}"
 
-if [[ -n "$BP_REWRITE_HOST" ]]; then
+if [[ -n "${BP_REWRITE_HOST:-}" ]]; then
     sed -i '' -e "s|^uri = \"https:\/\/buildpacks\.cloudfoundry\.org\(.*\)\"$|uri = \"http://$BP_REWRITE_HOST\1\"|g" "$full_path/buildpack.toml"
 fi
 
