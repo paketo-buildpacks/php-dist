8495fa778b9d59d0ed498469da74b1de6b32bec8
Arjun Sreedharan <asreedharan@pivotal.io>
2021-11-17 17:20:00 -0500

    Tests: Verify buildpack installs extensions correctly (#317)
    Protects against issues like https://github.com/paketo-buildpacks/packit/issues/245

---
+++ integration/extensions_loadable_test.go
@@ -0,0 +1,82 @@
+package integration_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/paketo-buildpacks/occam"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+)
+
+func testExtensionsLoadable(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect     = NewWithT(t).Expect
+		Eventually = NewWithT(t).Eventually
+		pack       occam.Pack
+		docker     occam.Docker
+	)
+
+	it.Before(func() {
+		pack = occam.NewPack()
+		docker = occam.NewDocker()
+	})
+
+	context("when building a simple app that loads extensions", func() {
+		var (
+			image     occam.Image
+			container occam.Container
+			name      string
+			source    string
+		)
+
+		it.Before(func() {
+			var err error
+			name, err = occam.RandomName()
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		it.After(func() {
+			Expect(docker.Container.Remove.Execute(container.ID)).To(Succeed())
+			Expect(docker.Image.Remove.Execute(image.ID)).To(Succeed())
+			Expect(docker.Volume.Remove.Execute(occam.CacheVolumeNames(name))).To(Succeed())
+			Expect(os.RemoveAll(source)).To(Succeed())
+		})
+
+		// PHP extensions are notorious in going wrong.
+		// Make sure this buildpack installs extensions correctly.
+		it("creates a working OCI image that can load extensions", func() {
+			var err error
+			source, err = occam.Source(filepath.Join("testdata", "app_loading_exts"))
+			Expect(err).NotTo(HaveOccurred())
+
+			var logs fmt.Stringer
+			image, logs, err = pack.WithNoColor().Build.
+				WithPullPolicy("never").
+				WithBuildpacks(
+					phpDistBuildpack,
+					buildPlanBuildpack,
+				).
+				Execute(name, source)
+			Expect(err).NotTo(HaveOccurred(), logs.String())
+
+			container, err = docker.Container.Run.
+				WithCommand("php app.php").
+				// tells php to use the ini config file in the app
+				WithEnv(map[string]string{"PHP_INI_SCAN_DIR": "/workspace/some-dir"}).
+				Execute(image.ID)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() string {
+				cLogs, err := docker.Container.Logs.Execute(container.ID)
+				Expect(err).NotTo(HaveOccurred())
+				return cLogs.String()
+			}).Should(
+				ContainSubstring("LOADED:zip"),
+			)
+		})
+	})
+}
