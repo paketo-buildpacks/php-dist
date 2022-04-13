d720c55ff3b14d62702357f369732b4ee04eab70
Sophie Wigmore <swigmore@vmware.com>
2022-04-13 12:10:42 -0400

    add filesystem SBOM capability

--- run/main.go
+++ run/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"os"
 
-	"github.com/paketo-buildpacks/packit/v2"
+	packit "github.com/paketo-buildpacks/packit/v2"
 	"github.com/paketo-buildpacks/packit/v2/cargo"
 	"github.com/paketo-buildpacks/packit/v2/chronos"
 	"github.com/paketo-buildpacks/packit/v2/draft"
 	"github.com/paketo-buildpacks/packit/v2/postal"
+	"github.com/paketo-buildpacks/packit/v2/sbom"
 	"github.com/paketo-buildpacks/packit/v2/scribe"
 	phpdist "github.com/paketo-buildpacks/php-dist"
 )
+
+type Generator struct{}
+
+func (f Generator) GenerateFromDependency(dependency postal.Dependency, path string) (sbom.SBOM, error) {
+	return sbom.GenerateFromDependency(dependency, path)
+}
 
 func main() {
 	buildpackYMLParser := phpdist.NewBuildpackYMLParser()
@@ -18,6 +25,7 @@ func main() {
 	entryResolver := draft.NewPlanner()
 	dependencyManager := postal.NewService(cargo.NewTransport())
 	environment := phpdist.NewEnvironment()
+	sbomGenerator := Generator{}
 
 	packit.Run(
 		phpdist.Detect(
@@ -28,6 +36,7 @@ func main() {
 			dependencyManager,
 			phpdist.NewPHPFileManager(),
 			environment,
+			sbomGenerator,
 			logEmitter,
 			chronos.DefaultClock,
 		),
